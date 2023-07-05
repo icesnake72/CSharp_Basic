9cdb66db7e939974075ef15ea807ef621e7ad4b5
icesnake72 <103027437+icesnake72@users.noreply.github.com>
2023-07-05 15:14:45 +0900

    async 예제 추가

---
+++ ex27/Async.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ex27
+{
+	public class FileCopy
+	{
+		string srcPath;
+		string destPath;
+        DelegateProc? callbackProc;
+
+        public FileCopy()
+		{
+			srcPath = string.Empty;
+			destPath = string.Empty;
+            callbackProc = null;
+        }
+
+		public async Task StartCopy(string src, string dest, DelegateProc callbackProc)
+		{
+            await CopyProgress(src, dest, callbackProc);
+        }
+
+        private async Task CopyProgress(string srcFile, string destFile, DelegateProc callbackProc)
+		{
+            FileInfo fileInfo = new FileInfo(srcFile);
+            if (!fileInfo.Exists)
+                return;
+
+            long totalBytes = fileInfo.Length;
+
+            using (FileStream srcFileStream = new FileStream(srcFile, FileMode.Open))
+            {
+                using (FileStream destFileStream = new FileStream(destFile, FileMode.Create))
+                {
+                    int bufferSize = 8192;
+                    byte[] buffer = new byte[bufferSize];
+
+                    int bytesRead;
+                    int offset = 0;
+                    while ((bytesRead = await srcFileStream.ReadAsync(buffer, 0, bufferSize)) > 0)
+                    {
+                        destFileStream.Write(buffer, 0, bytesRead);
+                        offset += bytesRead;
+                        if (callbackProc != null)
+                            callbackProc(offset, totalBytes);
+
+                        Thread.Sleep(1);
+                    }
+                }
+            }            
+        }
+
+    }
+}
+
--- ex27/Program.cs
+++ ex27/Program.cs
@@ -1,31 +1,119 @@
-﻿namespace ex27;
+﻿using System.Net;
+
+namespace ex27;
+
+public delegate void DelegateProc(long param1, long? param2);
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
-        LinkedList<int> list = new();
+        //LinkedList<int> list = new();
 
-        for(int i=0; i<5; i++)
-            list.AddLast(i);
+        //for(int i=0; i<5; i++)
+        //    list.AddLast(i+1);
 
-        Console.WriteLine(string.Join(" ", GetNumberInList(list)));
+        //Console.WriteLine(string.Join(" ", GetNumberInList(list)));
 
-        //Console.WriteLine(14*3*3);
+        ////Console.WriteLine(14*3*3);
 
-        // async & await
-        //await foreach(int number in list)
+        //// async & await
+        ////await foreach(int number in list)
+        //{
+        //    //await Console.WriteLine(string.Join(", ", ));
+        //    // await GetAsyncNumberInList(list);
+        //}
+
+        //List<int> newList = list.Where(x => x % 2 == 0).ToList();
+        //Console.WriteLine(string.Join(" ", newList));
+        //ShowList(list);
+
+
+        //Task t1 = AsyncMethod1();
+        //Console.WriteLine("AsyncMethod1()호출완료!");
+        //Task t2 = AsyncMethod2();
+        //Console.WriteLine("AsyncMethod2()호출완료!");
+
+        //await Task.WhenAll(t1, t2);
+
+
+        FileCopy copy = new();
+        Task tk = copy.StartCopy("/Users/eunbumkim/Documents/유튜브 배너.psd", "copy.psd", ShowDownloadProgress);
+        await Task.WhenAll(tk);
+
+        // Console.ReadLine();
+
+        Console.WriteLine("프로그램 종료 ");
+    }
+
+    public static void ShowDownloadProgress(long param1, long? param2)
+    {
+        if (param2 == null)
+            return;
+
+        if ( param2!=0 )
         {
-            //await Console.WriteLine(string.Join(", ", ));
-            // await GetAsyncNumberInList(list);
+            float progress = (param1 / (float)param2) * 100f;
+            Console.WriteLine($"{Math.Round(progress,2)}% progressing...");
+        }
+    }
+
+    public static async IAsyncEnumerable<int> GetRandomValue(int count)
+    {
+        Random rnd = new();
+        for(int i=0; i<count; i++)
+        {
+            yield return rnd.Next(101);
+        }
+    }
+
+    public static async Task AsyncMethod1()
+    {
+        await Task.Delay(1);
+
+        //using (var client = new WebClient())
+        //{
+        //    await client.DownloadFileTaskAsync(FormUrlEncodedContent, filename);
+        //}
+
+        await foreach(int num in GetRandomValue(5))
+        {
+            Thread.Sleep(100);
+            Console.WriteLine($"1 : {num}");
         }
 
-        ShowList(list);
-
-        Console.WriteLine("foreach()문 종료!!!!");
+        //await Task.Run(() =>
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Thread.Sleep(100);
+        //        Console.WriteLine($"1 : {i}");
+        //    }
+        //});
     }
+
+    public static async Task AsyncMethod2()
+    {
+        await Task.Delay(1);
+        await foreach (int num in GetRandomValue(5))
+        {
+            Thread.Sleep(100);
+            Console.WriteLine($"2 : {num}");
+        }
+
+        //await Task.Run( () =>
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Thread.Sleep(100);
+        //        Console.WriteLine($"2 : {i}");
+        //    }
+        //});
+    }
+
+
 
     public static IEnumerable<int> GetNumberInList(LinkedList<int> list)
     {
